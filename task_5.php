eef53ab060a765732628778c47bb6c42955f7b27
Md. Himel Ali <83213572+creativehimel@users.noreply.github.com>
2023-09-27 20:09:51 +0600

    Task 5 has been solved

--- task_5.php
+++ task_5.php
@@ -1,6 +1,21 @@
 <?php
 function generatePassword($length){
-    $data = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return substr(str_shuffle($data), 0, $length);
+    $random_characters = 3;
+   $lower_case = "abcdefghijklmnopqrstuvwxyz";
+   $upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   $numbers = "1234567890";
+   $symbols = "!@#$%^&*()_+";
+ 
+   $lower_case = str_shuffle($lower_case);
+   $upper_case = str_shuffle($upper_case);
+   $numbers = str_shuffle($numbers);
+   $symbols = str_shuffle($symbols);
+ 
+   $random_password = substr($lower_case, 0, $random_characters);
+   $random_password .= substr($upper_case, 0, $random_characters);
+   $random_password .= substr($numbers, 0, $random_characters);
+   $random_password .= substr($symbols, 0, $random_characters);
+ 
+   return substr(str_shuffle($random_password), 0, $length);
 }
 echo generatePassword(12);
